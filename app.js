d9628c6bbf57529865cb0bc326ee6e633374dce2
unknown <rebflora>
2021-09-10 15:03:54 +0100

    update

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const switcher = document.querySelector('.btn');
 switcher.addEventListener('click', function() {
     document.body.classList.toggle('dark-theme')
 
+
     var className = document.body.className;
     if(className == "light-theme") {
         this.textContent = "Dark";
@@ -13,6 +14,7 @@ switcher.addEventListener('click', function() {
     }
 
     console.log('current class name: ' + className);
+    
     
 });
 
