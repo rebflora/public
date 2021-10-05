544d3f5e00827682b7e86977728f36b6c5875ff9
unknown <rebflora>
2021-10-05 21:04:33 +0100

    updated

--- app.js
+++ app.js
@@ -56,3 +56,12 @@ const navbarScroll = () => {
     }
 };
 window.onscroll = navbarScroll;
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+      navigator.serviceWorker
+        .register("/serviceWorker.js")
+        .then(res => console.log("service worker registered"))
+        .catch(err => console.log("service worker not registered", err))
+    })
+  }
